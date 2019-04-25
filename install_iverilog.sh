1d8f50f99462daac18b7feb0647ab63ea2a5b2f2
Miguel Guerrero <miguel.a.guerrero@gmail.com>
2019-04-24 20:20:50 -0700

    improved

--- install_iverilog.sh
+++ install_iverilog.sh
@@ -2,6 +2,16 @@
 
 inst_dir=$HOME/iverilog
 
+export TARGET_ARCH=  #in mac-os if this is set, linking fails
+
+error_exit() {
+    echo "$1" 1>&2
+    exit 1
+}
+
+#check if wget is available, if not exit
+which wget || error_exit "install wget before proceeding please. Exiting.."
+
 #use this script to install from source in a local directory (no need for root
 #access). If the install directory is changed from the default above, make sure 
 #to update common/include.mk PATH variable accordingly or have the new path/bin
