d74afc85d720dc192d158d4b933ca3a6c54e1a8f
Miguel Guerrero <miguel.a.guerrero@gmail.com>
2019-04-26 17:16:58 -0700

    added counter example

---
+++ test/counter.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+#-------------------------------------------------------------------------------
+# (c) 2018 Miguel A. Guerrero all rights reserved
+#-------------------------------------------------------------------------------
+from chipo import *
+
+def genCounter(name='counter', *, cntW=8):
+    WIDTH = Parameter(cntW)
+    clk, rst_n = Clock(), Reset()
+    en, clr = Input() ** 2
+    max_val = Input(WIDTH)
+    eq, cnt = Output(), Output(WIDTH)
+    return Module(name) [
+            Combo() [
+                eq <= (cnt == max_val),
+            ],
+            Comment('clear and increment logic', 'goes here'),
+            Clocked(clk, rst_n) [
+                If (clr) [
+                    Comment('clear has priority'),
+                    cnt <= 0
+                ].Elif (en) [
+                    cnt <= cnt + 1
+                ]
+            ]
+        ].autoGen()
+
+
+
+if __name__=='__main__':
+    #start generation
+    cntr = genCounter()
+
+    #print verilog code for it
+    print(cntr.vlog(recursive=True))
