9614298a60d8ebfaffefab40876c1c76dd21355d
Miguel Guerrero <miguel.a.guerrero@gmail.com>
2019-06-03 07:11:31 -0700

    adding another pipeline test

---
+++ test_chipo/gold/pipe1.v
@@ -0,0 +1,49 @@
+//----------------------------------------------------------------------------
+// pipe
+//----------------------------------------------------------------------------
+module pipe 
+#(
+    parameter W = 8)
+(
+    input clk,
+    input [W-1:0] x,
+    input [W-1:0] y,
+    output reg [2*W+1:0] arith_s2,
+    output reg [2*W-1:0] out
+);
+
+reg [W:0] arith_stg1_s;
+reg [W-1:0] arith_stg1_x;
+reg [2*W+1:0] arith_stg2_s2;
+reg [W-1:0] arith_stg2_x;
+reg [2*W-1:0] arith_x2;
+
+// --- Stage 1 ---
+
+always @(posedge clk) begin : arith_stage1
+    arith_stg1_s <= x + y;
+    arith_stg1_x <= x;
+end
+
+// --- Stage 2 ---
+
+always @(posedge clk) begin : arith_stage2
+    arith_stg2_s2 <= arith_stg1_s * arith_stg1_s;
+    arith_stg2_x <= arith_stg1_x;
+end
+
+// --- Stage 3 ---
+
+always @(posedge clk) begin : arith_stage3
+    arith_x2 <= arith_stg2_x * arith_stg2_x;
+    arith_s2 <= arith_stg2_s2;
+end
+
+// x2:arith_x2, s2:arith_s2
+
+always @(*) begin
+    out <= arith_x2 - 1;
+end
+
+endmodule
+
---
+++ test_chipo/pipe1.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+#-------------------------------------------------------------------------------
+# (c) 2018 Miguel A. Guerrero all rights reserved
+#-------------------------------------------------------------------------------
+import sys
+sys.path.append("..") # add path to chipo and related
+from chipo import *
+
+clk = Clock()
+rst_n = Reset()
+W = Parameter(8)
+x, y = Input(W) ** 2
+s = Signal(W+1)
+s2 = Signal(2*W+2)
+x2 = Signal(2*W)
+out = Output(2*W)
+
+m = Module('pipe')
+pipe = Pipeline('arith', clk, rst_n, keep=[s2, x2]) [
+    s <= x+y,
+    ...,
+    s2 <= s*s,
+    ...,
+    x2 <= x*x   # note that x is transfered through 2nd pipe stage
+]
+
+m += pipe.expand()
+m += Comment(', '.join([f"{x}:{v.name}" for x,v in pipe.outs.getDict().items()]))
+m += Assign(out, pipe.outs.x2 - 1)
+
+m.autoGen()
+
+print(m.vlog())
