a154188f0ced2553f760f28809b4c778006d6dce
Miguel Guerrero <miguel.a.guerrero@gmail.com>
2019-06-21 09:28:11 -0700

    added license header

--- regs.py
+++ regs.py
@@ -1,6 +1,24 @@
 #!/usr/bin/env python3
-#-------------------------------------------------------------------------------
-# (c) 2018 Miguel A. Guerrero all rights reserved
+#------------------------------------------------------------------------------
+# Copyright (c) 2018-Present, Miguel A. Guerrero
+# All rights reserved.
+#
+# This is free software released under GNU Lesser GPL license version 3.0 (LGPL 3.0)
+#
+# See http://www.gnu.org/licenses/lgpl-3.0.txt for a full text
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND 
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+# IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE 
+# OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
+# OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Please send bugs and suggestions to: miguel.a.guerrero@gmail.com
 #-------------------------------------------------------------------------------
 from chipo import *
 import g2p as g
